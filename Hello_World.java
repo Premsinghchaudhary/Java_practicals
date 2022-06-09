5d125221cd937100463f44ef66bb0bb4efefc70e
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-09 07:55:04 -0700

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,8 @@
+class Hello_World
+{
+  //  
+ public static void main(String args[]){ //USING MAIN METHOD
+
+  System.out.println(" HELLO WORLD ");
+}
+}
---
+++ add_two_numbers.java
@@ -0,0 +1,9 @@
+class add_two_numbers{
+public static void main (String args[]){
+int a,b,sum;   // using datatype
+int a=20,b=90;
+sum=a+b;       // sum of a and b 
+System.out.println("The sum of two number is = "+sum);
+
+}
+}
