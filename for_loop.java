31ed8a7aeeb16aefa8e04d1dc052c6aab73078c7
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-13 23:20:21 +0530

    Add files via upload

--- for_loop.java
+++ for_loop.java
@@ -1,13 +1,15 @@
 package loops;
-import java.util.*;
 
-public class for_loop {
-	public static void main (String agrs[] ) {
+public class for_loop
+{
+	public static void main (String agrs[] ) 
+	{
 		
 		int i;  // declartion varaiable 
 		// using for loop
-		for(i=1;i<790;i++) {
-			System.out.print(i+"\t");
+		for(i=1;i<790;i++) 
+		{
+			System.out.print("\t"+i);
 			
 		}
 		
