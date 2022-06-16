4816eae8495e348902f420ea32e0823bd9f51c88
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 22:52:19 +0530

    Update loop_nested.java

--- loop_nested.java
+++ loop_nested.java
@@ -2,12 +2,13 @@ package loops;
 
 public class loop_nested {
 	public static void main (String args[]) {
-		int i,j;
-		for( i=1;i<=5;i++)
+		int i,j; //variable declared 
+		for( i=1;i<=5;i++) //using for loop 'row'
 		{
-			     for( j=5;j>=i;j--)
+			     for( j=5;j>=i;j--) //using for loop 'column'
 			     {
-			    	   if( j%2==0) {
+			    	   if( j%2==0) //using statement
+				   { 
 			    		   System.out.print( "0"); 
 			    	   }
 			    	   else {
