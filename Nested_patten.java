1b4335396a07685c73fc3555ae0d82166e02101d
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 14:19:24 +0530

    Update Nested_patten.java

--- Nested_patten.java
+++ Nested_patten.java
@@ -3,14 +3,14 @@ package loops;
 public class Nested_patten {
 
 	public static void main(String[] args) {
-		int i,j;
-		for(i=1;i<=5;i++ ) 
+		int i,j; // declared the variable
+		for(i=1;i<=5;i++ ) //using nested loop
 		{
 			for(j=1;j<=i;j++ ) 
 			{
 				System.out.print("*" );
 			}
-			   System.out.println();
+			   System.out.println();    
 		}
 
 	}
