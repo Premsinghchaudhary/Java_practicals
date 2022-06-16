2002bf21389e966da1c66c4f5b88108c79c71b5f
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 22:34:35 +0530

    Update nestedloop.java

--- nestedloop.java
+++ nestedloop.java
@@ -2,21 +2,21 @@ package loops;
 
 public class nestedloop {
 	public static void main ( String args []){
-		int i,j;
-		for( i=1;i<=5;i++)
+		int i,j;//declared the variable and datatype
+		for( i=1;i<=5;i++) //using for loop i is for row
 		{
-			  for(j=1;j<=i;j++)
+			  for(j=1;j<=i;j++) //using for loop j is for column
 			  {
-				  if( j%2==0)              
+				  if( j%2==0)              //using condition statement
 				  {
-					  System.out.print("0");
+					  System.out.print("0"); //print 0''
 				  }
 				  else 
 				  {
-					System.out.print("1");  
+					System.out.print("1");  //print '1'
 				  }
 			  }
-		     System.out.println();
+		     System.out.println(); 
 		}
 		
 	}
