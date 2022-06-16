1d59a2ffe8d81a123e5557cc44fcd541884fb241
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 21:25:59 +0530

    Update switch_case.java

--- switch_case.java
+++ switch_case.java
@@ -3,40 +3,40 @@
 import java.util.Scanner; 
 class switch_case
 {
-public static void main(String args[])
+public static void main(String args[])  //main method 
 {
-  int x,y,choice;
+  int x,y,choice; //declartion varaiable
 
- System.out.println("Enter the two numbers ");
+ System.out.println("Enter the two numbers "); //get the number from user 
    
- Scanner sc = new Scanner(System.in);
+ Scanner sc = new Scanner(System.in); //scanner object
  
- x=sc.nextInt();
+ x=sc.nextInt(); 
  y= sc.nextInt();
 
 
- System.out.println("Enter the choice/n 1 Add :+n/ 2 sub:+/n 3 Mul:+/n 4 Division :+/n 5 Modulas ");
- choice=sc.nextInt();
- switch(choice)
+ System.out.println("Enter the choice/n 1 Add :+n/ 2 sub:+/n 3 Mul:+/n 4 Division :+/n 5 Modulas ");  //print thw choice from user select the no the get the result
+ choice=sc.nextInt();  //choice object by using scanner
+ switch(choice)//using switch case 
 {
   case 1:
-  System.out.println(" The addition of two numbers is : "+(x+y) );
+  System.out.println(" The addition of two numbers is : "+(x+y) ); //for addition 
   break;
 
   case 2:
-  System.out.println(" The sub of two numbers is : "+(x-y) );
+  System.out.println(" The sub of two numbers is : "+(x-y) );  //for substraction
   break;
  
   case 3:
-  System.out.println(" The multipicaltion  of two numbers is : "+(x*y) );
+  System.out.println(" The multipicaltion  of two numbers is : "+(x*y) ); //for multiplication 
   break;
 
   case 4:
-  System.out.println(" The Division of two numbers is : "+(x/y) );
+  System.out.println(" The Division of two numbers is : "+(x/y) ); //for division
   break;
   
   case 5:
-  System.out.println(" The Modulas of two numbers is : "+(x%y) );
+  System.out.println(" The Modulas of two numbers is : "+(x%y) ); //for modulas
   break;
 
  default:
