22acb8683e668e47b7b4d4f3ad9366df542513f4
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 14:45:53 +0530

    Update P_print_using_nested_loop.java

--- P_print_using_nested_loop.java
+++ P_print_using_nested_loop.java
@@ -4,16 +4,16 @@ public class P_print_using_nested_loop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int i,j;
-        for( i=1;i<8;i++)
+        int i,j;     //declaration of variabla		
+        for( i=1;i<8;i++)  //using for loop
         {
-        	for(j=1;j<6;j++) 
+        	for(j=1;j<6;j++) //using for loop
         	{
-        	        if( i==1||i==4)
+        	        if( i==1||i==4) //uisng condition statment ' else if'
         	        {
         			
                   	
-        	              	if( j==5)
+        	              	if( j==5)  //using condition statement ' if '
                             {
                           	System.out.print(" ");
         		            }
@@ -24,21 +24,21 @@ public class P_print_using_nested_loop {
         	        else if( i==2||i==3)
         	        {
         	        	     if( j==1||j==5) {
-        	        	    	 System.out.print("*");
+        	        	    	 System.out.print("*"); //using for '*' print
         	        	     }
         	        	     else {
-        	        	    	 System.out.print(" ");
+        	        	    	 System.out.print(" "); //uisng for space
         	        	     }
         	        }
         	        else
         	        { 
         	                 if( j==1) {
-        	                	 System.out.print("*");
+        	                	 System.out.print("*"); //using for print
         	                 }
         	        	   
         	        }
         	}
-        	System.out.println();
+        	System.out.println(); //using for space
        }
         	              	
         			
