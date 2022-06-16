cd69e2d895455122447d4370074b824c0c761729
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 22:51:00 +0530

    Update fibonacci_series.java

--- fibonacci_series.java
+++ fibonacci_series.java
@@ -4,18 +4,20 @@ package Function;
 
 
 public class fibonacci_series {
-      public static void main ( String args[]) {
-    	  
-    	  fib();
+      public static void main ( String args[]) 
+      {  
+    	  fib(); //calling the function
       }
-      public static void fib(){
-    	  int i,n=0,n1=1,n2,c=10;
-    	  System.out.println("n = "+n);
-    	  System.out.println("n1 = "+n1);
+      public static void fib()//create the function 
+      { 
+    	  int i,n=0,n1=1,n2,c=10; //declared the variable
+    	  System.out.println("n = "+n); //print the value of n
+    	  System.out.println("n1 = "+n1);//print the value of n1
     	  
-    	  for( i=2;i<c;i++) {
-    		  n2=n+n1;
-    		  System.out.println("n2 = "+n2);  
+    	  for( i=2;i<c;i++) //using for loop 
+        {
+    		  n2=n+n1; //sense of humar
+    		  System.out.println("n2 = "+n2);//print the value of n2  
     		  n=n1;
     		  n1=n2;
     		  }
