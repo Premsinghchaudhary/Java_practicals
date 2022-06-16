35dd7c594addc6d60d7ec314d47a7d4466cd5e64
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 22:46:57 +0530

    Update multipication_table.java

--- multipication_table.java
+++ multipication_table.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class multipication_table {
 
 	public static void main (String args []) {
-		int x;
-	Scanner sc = new Scanner(System.in);
+		int x;//declaered the variable 
+	Scanner sc = new Scanner(System.in); //scanner 
 	
-	System.out.print( " enter the number : ");
-	x=sc.nextInt();
+	System.out.print( " enter the number : "); //get the number from user 
+	x=sc.nextInt(); 
 //using for loop
 	
 		for( int i=1;i<11;i++) {
-			System.out.println( x + " * " +i+" = " +( x*i));
+			System.out.println( x + " * " +i+" = " +( x*i)); //calcualte the everything in this code and get the result 
 		}
 	}
 }
