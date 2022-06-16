f4f74997c7ac5a6e99b65bebe0628d3771848c5d
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 14:15:38 +0530

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -3,6 +3,6 @@ class Hello_World
   //  
  public static void main(String args[]){ //USING MAIN METHOD
 
-  System.out.println(" HELLO WORLD "); //print the hello_world
+  System.out.println(" HELLO WORLD "); //print the ......
 }
 }
