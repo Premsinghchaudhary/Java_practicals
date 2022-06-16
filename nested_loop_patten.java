c29a961d515e2dd14a520c635612b74547c9c39d
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 22:36:26 +0530

    Update nested_loop_patten.java

--- nested_loop_patten.java
+++ nested_loop_patten.java
@@ -2,13 +2,14 @@ package loops;
 
 public class nested_loop_patten {
 
-	public static void main(String[] args) {
-		int i,j;
-		for(i=1;i<=5;i++ ) 
+	public static void main(String[] args) //main method
+	{
+		int i,j; //declared the variable
+		for(i=1;i<=5;i++ ) //using for loop i is used for row
 		{
-			       for( j=5;j>=i;j--) 
+			       for( j=5;j>=i;j--)  //using foe loop j is used for column
 			       {
-			    	   System.out.print("*");
+			    	   System.out.print("*"); //print the *
 			       }
 		              System.out.println();
 		}
