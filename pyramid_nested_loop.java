4d60ca7645694fc8b908c6d9749fb46e58c11df9
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 21:34:51 +0530

    Update pyramid_nested_loop.java

--- pyramid_nested_loop.java
+++ pyramid_nested_loop.java
@@ -1,19 +1,19 @@
 package loops;
 
-public class pyramid_nested_loop {
+public class pyramid_nested_loop {     // reverse pyramid
 
 	public static void main(String[] args) {
-    int i,j,k;
-    for( i=5;i>=1;i--) 
+    int i,j,k;//declartion variable
+    for( i=5;i>=1;i--) //using for loop i is use for row
     {
     	     for(k=4;k>=i;k--)//k for space 
     	     {
-    	    	 System.out.print(" ");
+    	    	 System.out.print(" "); 
     	     }
-    	     for( j=1;j<=2*i-1;j++) {
-    	    	 System.out.print("*");
+    	     for( j=1;j<=2*i-1;j++) { //j is use fro colum
+    	    	 System.out.print("*"); //print star*
       	     }
-                 System.out.println();
+                 System.out.println(); 
     }
     
     
