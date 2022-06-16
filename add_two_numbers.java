2285d045a3251602ed5ec5cdb810835ba8bd96bc
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 21:57:52 +0530

    Update add_two_numbers.java

--- add_two_numbers.java
+++ add_two_numbers.java
@@ -1,10 +1,11 @@
 class add_two_numbers{
-public static void main (String args[] ){
-int x,y,sum;   // using datatype
+public static void main (String args[] ) //main method 
+{
+int x,y,sum;   // using datatype is int and variable declartion x,y
   x=20;
   y=90;
-sum=(x+y);       // sum of x and y
-System.out.println("The sum of two number is = " + sum);
+sum=(x+y);       // calculate the  sum of x and y
+System.out.println("The sum of two number is = " + sum);//print the value of sum
 
 }
 }
