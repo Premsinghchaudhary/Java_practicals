e5841697a20be6f823d2aae3fa246fe5414a4491
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 15:05:58 +0530

    Update Swapping.java

--- Swapping.java
+++ Swapping.java
@@ -6,31 +6,31 @@ import java.util.Scanner;
 
 public class fun_demo {
 
-	public static void main(String[] args) {
-		accept_no();
+	public static void main(String[] args) {  
+		accept_no();  //calling the accept_no
 	
 
 	}
-	  public static void accept_no() {
-		  int a,b;
-		  Scanner sc =new Scanner(System.in);
-		  System.out.println(" Enter the two number :");
+	  public static void accept_no() {//create function method 
+		  int a,b; //variable declartion and datatype is int
+		  Scanner sc =new Scanner(System.in); //create scanner 
+		  System.out.println(" Enter the two number :"); // accept the no from  user 
 		  a=sc.nextInt();
 		  b=sc.nextInt();
-		  System.out.println(" the value of a is :"+a);
-		  System.out.println(" the value of b is :"+b);
+		  System.out.println(" the value of a is :"+a); //print for a value 
+		  System.out.println(" the value of b is :"+b); //print fro b value 
 		  swapping(a,b);
 		
 		
 
 	  }
-	  public static void swapping(int x,int y){
-		  x=x+y;
-		  y=x-y;
-		  x=x-y;
+	  public static void swapping(int x,int y){ //create swappong method 
+		  x=x+y; // swapping the numbers 
+		  y=x-y;// swapping the numbers
+		  x=x-y;// swapping the numbers
 		  
-		  System.out.println(" The swapping of b=x  :" + x);
-		  System.out.println(" The swapping of a=y  :" +y);
+		  System.out.println(" The swapping of b=x  :" + x); //print  the value of x  after swapping 
+		  System.out.println(" The swapping of a=y  :" +y);  //print  the value of y after swapping
 		  
 	  }
 
