cc217c58ac44f37080b1c3b7192ca0e189ba95be
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 14:40:48 +0530

    Update Nested_patten.java

--- Nested_patten.java
+++ Nested_patten.java
@@ -4,7 +4,7 @@ public class Nested_patten {
 
 	public static void main(String[] args) {
 		int i,j; // declared the variable
-		for(i=1;i<=5;i++ ) //using nested loop
+		for(i=1;i<=5;i++ ) //using for loop
 		{
 			for(j=1;j<=i;j++ ) 
 			{
