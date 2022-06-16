4591e87cf038ce1831b00785a7757d00aa1c2d12
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 14:57:38 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -4,53 +4,53 @@ import java.util.Scanner;
   {  
    public static void main (String  args[]) 
   {
-   int roll,eng,hin,maths,eco,sci,per,marks;
-   String name ;
+   int roll,eng,hin,maths,eco,sci,per,marks; //variable and data type declared
+   String name ;                       // data type is string use for name 
 
-    Scanner sc=new Scanner(System.in);
+    Scanner sc=new Scanner(System.in);  //create scanner object 
 
    System.out.println("write your Student name  ");
-   name =sc.nextLine();
-  System.out.println(" Student name is = " + name );
+   name =sc.nextLine();              
+  System.out.println(" Student name is = " + name ); //for print the  student name 
 
 
    System.out.println("write the  roll of Student " );
    roll =sc.nextInt();
-  System.out.println(" Student roll no.  = " + roll );
+  System.out.println(" Student roll no.  = " + roll );  // for print the student roll no 
  
 
 
 
    System.out.println("write the your marks in english " );
    eng =sc.nextInt();
-  System.out.println(" english  = " + eng );
+  System.out.println(" english  = " + eng );   // for print the marks in english 
     
    
 
    System.out.println("write the your marks in hindi " );
    hin =sc.nextInt();
-  System.out.println(" hindi  = " + hin );
+  System.out.println(" hindi  = " + hin );// for print the marks in hindi
 
 
    System.out.println("write the your marks in maths " );
    maths =sc.nextInt();
-  System.out.println(" maths  = " + maths );
+  System.out.println(" maths  = " + maths );// for print the marks in maths
 
 
    System.out.println("write the your marks in economics" );
    eco =sc.nextInt();
-  System.out.println(" economics  = " + eco );
+  System.out.println(" economics  = " + eco );// for print the marks in economics
 
 
    System.out.println("write the your marks in science " );
    sci =sc.nextInt();
-  System.out.println(" science = " + sci );
+  System.out.println(" science = " + sci );// for print the marks in science 
 
 
-    marks=eng+hin+maths+eco+sci; 
-    System.out.println(" The total sum of = "+marks);  
-   per = marks/5;
-  System.out.println("  your percent is =   " +per+ "%" );
+    marks=eng+hin+maths+eco+sci;   //calculate sum of the  marks
+    System.out.println(" The total sum of = "+marks);     // print sum of marks
+   per = marks/5;   // calculate percentage of marks 
+  System.out.println("  your percent is =   " +per+ "%" ); //print % of marks
 
 
   }
