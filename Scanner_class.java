dcab32d9557d97df613b883e961930028d803852
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 14:50:40 +0530

    Update Scanner_class.java

--- Scanner_class.java
+++ Scanner_class.java
@@ -1,23 +1,24 @@
-  import java.util.Scanner;
+  import java.util.Scanner;     
   class Scanner_class
 {
   public static void main( String args []) //main method 
 {
-  int num1,num2,sum;                        //use datatype 
+  int num1,num2,sum;                        //use datatype and variable declartion
 
   Scanner sc =new Scanner(System.in);      //using a Scanner
 
   System.out.println( " write your first number");
   num1=sc.nextInt();                         //create scanner object
-  System.out.println(" num1 = "+num1);
+  System.out.println(" num1 = "+num1);      // using for num1 value print 
 
 
   System.out.println( " write your second number");
-  num2=sc.nextInt();
-  System.out.println(" num2 = "+num2);
+  num2=sc.nextInt();                       //create scanner object
+  System.out.println(" num2 = "+num2);    // using for num2 value print 
+
 
   sum=num1+num2;
-  System.out.println(" The sum of two number is = "+sum); 
+  System.out.println(" The sum of two number is = "+sum);  //print the value of sum
 
 
       
