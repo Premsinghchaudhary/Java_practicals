3795c16d5d7a4407c4a1c196f7d744b969864e4e
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 22:58:08 +0530

    Update find_fatorial.java

--- find_fatorial.java
+++ find_fatorial.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class find_fatorial {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int n,fact=1;
-		System.out.println(" enter the number");
+	public static void main(String[] args) //main method
+	{
+		int n,fact=1;//varaible declared
+		System.out.println(" enter the number");//get the input from user 
 		
-		Scanner sc= new Scanner(System.in);
+		Scanner sc= new Scanner(System.in); //scanner
 		n=sc.nextInt();
-		 while(n>0) 
+		 while(n>0) //using while loop
 		 {
-		    fact=fact*n;
-		    n--;
+		    fact=fact*n; //calculate the fatorial 
+		    n--;  // decsement 
 	     }
-          System.out.println( " factorial is = "+fact);
+          System.out.println( " factorial is = "+fact); //print the fatorial value 
 	}
          
 }
