e57cbb5f6945961f79fb66d0a7d36fa1e99c70ff
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 21:36:47 +0530

    Update Terinery_opt.java

--- Terinery_opt.java
+++ Terinery_opt.java
@@ -12,11 +12,11 @@ Scanner sc=new Scanner (System.in);
 
     n1=sc.nextInt();
         n2=sc.nextInt();
-        System.out.println("The n1 =  " +n1);
-        System.out.println("The n2 =  " +n2);
+        System.out.println("The n1 =  " +n1); //print n1 value 
+        System.out.println("The n2 =  " +n2);//print n2 value 
         
-       res= ( n1 > n2) ? n1 : n2;
-       System.out.println(" The result is = "+res);
+       res= ( n1 > n2) ? n1 : n2; //usine ternierny opr.
+       System.out.println(" The result is = "+res); //print the final result
             
 
 }
