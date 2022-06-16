2dec219428772a485d6f9943fa937210f0f73b50
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 22:55:21 +0530

    Update for_loop.java

--- for_loop.java
+++ for_loop.java
@@ -5,11 +5,11 @@ public class for_loop
 	public static void main (String agrs[] ) 
 	{
 		
-		int i;  // declartion varaiable 
+		int i;  // declartion varaiable  and datatype is int
 		// using for loop
 		for(i=1;i<790;i++) 
 		{
-			System.out.print("\t"+i);
+			System.out.print("\t"+i); //print the * 
 			
 		}
 		
