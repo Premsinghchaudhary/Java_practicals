6355767f309aff317dae722671ba912aa4571e41
Premsinghchaudhary <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-16 22:31:51 +0530

    Update cube.java

--- cube.java
+++ cube.java
@@ -5,22 +5,22 @@
  {
      public static void main(String args[])
     {
-  int a;
+  int a; //declared the variable 
 
- System.out.println("Enter the  numbers ");
+ System.out.println("Enter the  numbers ");//get the number from user 
    
- Scanner sc = new Scanner(System.in);
+ Scanner sc = new Scanner(System.in);//scanner 
  
- a=sc.nextInt();
-
- if( a==0)
+ a=sc.nextInt(); //pass the value of a in scanner 
+// using condition statemnet 'else if'
+ if( a==0) 
 {
  System.out.println(" neither positive or nor negative = not defined ");
 
 }
 else if(a%2==0 )
 {
- System.out.println("value of a is even now find a cube :" + (a*a*a) );
+ System.out.println("value of a is even now find a cube :" + (a*a*a) ); //calculate the cube and print
 }
 else
 {
